5aeed8aa14adad94342b85571f30252937c114b3
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2019-08-28 12:32:02 +0200

    Minor, add test on name mangling of private multi-file part members

--- compiler/tests/org/jetbrains/kotlin/codegen/BytecodeTextTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/BytecodeTextTestGenerated.java
@@ -3149,6 +3149,11 @@ public class BytecodeTextTestGenerated extends AbstractBytecodeTextTest {
         public void testOptimizedMultifileClassFacadeMethods() throws Exception {
             runTest("compiler/testData/codegen/bytecodeText/multifileClasses/optimizedMultifileClassFacadeMethods.kt");
         }
+
+        @TestMetadata("privateFunctionName.kt")
+        public void testPrivateFunctionName() throws Exception {
+            runTest("compiler/testData/codegen/bytecodeText/multifileClasses/privateFunctionName.kt");
+        }
     }
 
     @TestMetadata("compiler/testData/codegen/bytecodeText/notNullAssertions")
--- compiler/tests/org/jetbrains/kotlin/codegen/ir/IrBytecodeTextTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/codegen/ir/IrBytecodeTextTestGenerated.java
@@ -3119,6 +3119,11 @@ public class IrBytecodeTextTestGenerated extends AbstractIrBytecodeTextTest {
         public void testOptimizedMultifileClassFacadeMethods() throws Exception {
             runTest("compiler/testData/codegen/bytecodeText/multifileClasses/optimizedMultifileClassFacadeMethods.kt");
         }
+
+        @TestMetadata("privateFunctionName.kt")
+        public void testPrivateFunctionName() throws Exception {
+            runTest("compiler/testData/codegen/bytecodeText/multifileClasses/privateFunctionName.kt");
+        }
     }
 
     @TestMetadata("compiler/testData/codegen/bytecodeText/notNullAssertions")
