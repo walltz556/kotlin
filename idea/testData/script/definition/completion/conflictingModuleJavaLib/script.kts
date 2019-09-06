5a680e5fc5602c9c88cecf95128953d0a6fd27d9
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2019-09-06 15:08:40 +0300

    Minor: extract classes

--- idea/testData/script/definition/completion/conflictingModuleJavaLib/script.kts
+++ idea/testData/script/definition/completion/conflictingModuleJavaLib/script.kts
@@ -2,6 +2,7 @@ staticMeth<caret>
 
 // DEPENDENCIES: classpath:lib-classes; sources:lib-source
 // CONFLICTING_MODULE
+
 // EXIST: staticMethod
 // INVOCATION_COUNT: 2
 // NOTHING_ELSE
---
+++ idea/testData/script/definition/order/customLibrary/lib/lib.kt
@@ -0,0 +1,3 @@
+package custom.library
+
+fun doStuff() {}
